06c997ad59ef821c7a239aae98fe2487cf89ac82
Mercen <mercen@mercen.net>
2023-01-31 11:18:40 +0900

    Initial Commit

---
+++ Rolling Root/Rolling_RootApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rolling_RootApp.swift
+//  Rolling Root
+//
+//  Created by Mercen on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct Rolling_RootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
